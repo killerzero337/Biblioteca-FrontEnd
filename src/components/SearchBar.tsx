86b0bd2c5850fed54402f4d31772b5b0f7ae4245
José Manuel Garrido González <metalmaster37yt@gmail.com>
2025-03-08 22:52:55 +0100

    fix: añadiendo labels para mayor usabilidad del filtro

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -23,9 +23,14 @@ const SearchBar: React.FC<SearchBarProps> = ({
 }) => {
   return (
     <div className="flex justify-center relative top-11 z-50 w-full">
-      <div className="w-full max-w-6xl rounded-xl bg-Dark-Violet md:h-40 h-90 bg-[url(../assets/bg-shorten-mobile.svg)] md:bg-[url(../assets/bg-shorten-desktop.svg)] bg-no-repeat bg-cover p-6">
+      <div className="w-full max-w-6xl rounded-xl bg-Dark-Violet md:h-50 h-120 bg-[url(../assets/bg-shorten-mobile.svg)] md:bg-[url(../assets/bg-shorten-desktop.svg)] bg-no-repeat bg-cover p-6">
+        
         <div className="flex flex-col md:flex-row items-center gap-4 md:gap-6">
+          <label className="sr-only " htmlFor="busqueda">
+            Buscar libro
+          </label>
           <input
+            id="busqueda"
             type="text"
             value={busqueda}
             onChange={onBusquedaChange}
@@ -39,55 +44,85 @@ const SearchBar: React.FC<SearchBarProps> = ({
 
         {/* Filtros */}
         <div className="grid grid-cols-2 gap-4 md:flex md:flex-row justify-center items-center mt-6 md:grid-cols-none">
-          <select
-            name="autor"
-            onChange={(e) => onFiltroChange("autor", e.target.value)}
-            className="p-2 border rounded-lg bg-white w-full md:w-auto"
-          >
-            <option value="">Filtrar por autor</option>
-            {autores.map((autor, index) => (
-              <option key={index} value={autor}>
-                {autor}
-              </option>
-            ))}
-          </select>
+          <div className="flex flex-col">
+            <label htmlFor="autor" className="mb-1 text-white">
+              Filtrar por autor
+            </label>
+            <select
+              id="autor"
+              name="autor"
+              onChange={(e) => onFiltroChange("autor", e.target.value)}
+              className="p-2 border rounded-lg bg-white w-full md:w-auto"
+            >
+              <option value="">Seleccionar autor</option>
+              {autores.map((autor, index) => (
+                <option key={index} value={autor}>
+                  {autor}
+                </option>
+              ))}
+            </select>
+          </div>
 
-          <select
-            name="genero"
-            onChange={(e) => onFiltroChange("genero", e.target.value)}
-            className="p-2 border rounded-lg bg-white w-full md:w-auto"
-          >
-            <option value="">Filtrar por género</option>
-            {generos.map((genero, index) => (
-              <option key={index} value={genero}>
-                {genero}
-              </option>
-            ))}
-          </select>
+          <div className="flex flex-col">
+            <label htmlFor="genero" className="mb-1 text-white">
+              Filtrar por género
+            </label>
+            <select
+              id="genero"
+              name="genero"
+              onChange={(e) => onFiltroChange("genero", e.target.value)}
+              className="p-2 border rounded-lg bg-white w-full md:w-auto"
+            >
+              <option value="">Seleccionar género</option>
+              {generos.map((genero, index) => (
+                <option key={index} value={genero}>
+                  {genero}
+                </option>
+              ))}
+            </select>
+          </div>
 
-          <select
-            name="disponibilidad"
-            onChange={(e) => onFiltroChange("disponibilidad", e.target.value)}
-            className="p-2 border rounded-lg bg-white w-full md:w-auto"
-          >
-            <option value="">Filtrar por disponibilidad</option>
-            <option value="true">Disponible</option>
-            <option value="false">No disponible</option>
-          </select>
+          <div className="flex flex-col">
+            <label htmlFor="disponibilidad" className="mb-1 text-white">
+              Filtrar por disponibilidad
+            </label>
+            <select
+              id="disponibilidad"
+              name="disponibilidad"
+              onChange={(e) => onFiltroChange("disponibilidad", e.target.value)}
+              className="p-2 border rounded-lg bg-white w-full md:w-auto"
+            >
+              <option value="">Seleccionar disponibilidad</option>
+              <option value="true">Disponible</option>
+              <option value="false">No disponible</option>
+            </select>
+          </div>
 
-          {/* Filtro por fecha desde */}
-          <input
-            type="date"
-            onChange={(e) => onFiltroChange("desde", e.target.value)}
-            className="p-2 border rounded-lg bg-white w-full md:w-auto"
-          />
+          <div className="flex flex-col">
+            <label htmlFor="fechaDesde" className="mb-1 text-white">
+              Filtrar desde
+            </label>
+            <input
+              id="fechaDesde"
+              type="date"
+              value={fechaDesde}
+              onChange={(e) => onFiltroChange("desde", e.target.value)}
+              className="p-2 border rounded-lg bg-white w-full md:w-auto"
+            />
+          </div>
 
-          {/* Filtro por fecha hasta */}
-          <input
-            type="date"
-            onChange={(e) => onFiltroChange("hasta", e.target.value)}
-            className="p-2 border rounded-lg bg-white w-full md:w-auto"
-          />
+          <div className="flex flex-col">
+            <label htmlFor="fechaHasta" className="mb-1 text-white">
+              Filtrar hasta
+            </label>
+            <input
+              id="fechaHasta"
+              type="date"
+              value={fechaHasta}
+              onChange={(e) => onFiltroChange("hasta", e.target.value)}
+              className="p-2 border rounded-lg bg-white w-full md:w-auto"
+            />
+          </div>
         </div>
       </div>
     </div>
